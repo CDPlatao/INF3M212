039ae168d548128306786d7af23f62d9c0f6ec70
Chrigor <chrigorgamer@gmail.com>
2022-12-13 12:00:50 -0300

    Arquivo

--- src/projeto/Compras.java
+++ src/projeto/Compras.java
@@ -1,9 +1,10 @@
 package projeto;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
 
-public class Menus {
+public class Compras {
     static ArrayList<String> listacompras = new ArrayList<>();
     static Scanner leia = new Scanner(System.in);
 
@@ -51,23 +52,59 @@ public class Menus {
     }// Menu
 
     public static void adicionar() {
-        String add;
+        Scanner ler = new Scanner(System.in);
+        String add, item;
         do {
+            System.out.println("\n --- Adicionar itens --- ");
             System.out.print("Informe o item aqui: ");
-            listacompras.add(leia.nextLine());
+            item = ler.nextLine();
+            if (!veritem(item)) {
+                listacompras.add(item);
+            } else {
+                System.out.println("O item ja existente!. ");
+            }
             System.out.println("Deseja adicionar novos itens ? (S/N)");
             add = leia.next();
         } while (add.equalsIgnoreCase("s"));
     }// Adicionar
 
     public static void remover() {
-        System.out.print("Informe o item aqui: ");
-        listacompras.remove(leia.nextLine());
+        Scanner ler = new Scanner(System.in);
+        String remove, item;
+        do {
+            System.out.println("\n --- Remover itens --- ");
+            System.out.print("Informe o item aqui: ");
+            item = ler.nextLine();
+            if (veritem(item)) {
+                listacompras.remove(item);
+            } else {
+                System.out.println(" Você não possui este item no carrinho!. ");
+            }
+            System.out.println("Deseja remover novos itens ? (S/N)");
+            remove = leia.next();
+        } while (remove.equalsIgnoreCase("s"));
     }// Remover
 
     public static void imprimir() {
-        System.out.println(listacompras.toString());
+        System.out.println("\n --- Itens no carrinho ---");
+        if (listacompras.isEmpty()) {
+            System.out.println(" O carrinho está vazio!.");
+        } // If
+
+        Collections.sort(listacompras);
+        for (String listacompra : listacompras) {
+            System.out.println(" " + listacompra);
+        } // For
     }// Imprimir
+
+    public static boolean veritem(String item) {
+        for (String itemlista : listacompras) {
+            if (itemlista.equalsIgnoreCase(item)) {
+                return true;
+            }
+        }
+        return false;
+    }// VerItem
 
     public static int leiaInt() {
         Scanner leia = new Scanner(System.in);
